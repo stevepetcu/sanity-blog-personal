8cfa64f77ee8307502e9142a5f108e5c75640887
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-05-12 15:49:44 +0100

    ✨ Add alt and title to author avatar.

--- components/AuthorAvatar.tsx
+++ components/AuthorAvatar.tsx
@@ -7,18 +7,19 @@ export default function AuthorAvatar(props: Author) {
   return (
     <div className="flex items-center">
       <div className="relative mr-4 h-12 w-12">
-        <Image
-          src={
-            picture?.asset?._ref
-              ? urlForImage(picture).height(96).width(96).fit('crop').url()
-              : 'https://source.unsplash.com/96x96/?face'
-          }
-          className="rounded-full"
-          height={96}
-          width={96}
-          // @TODO add alternative text to avatar image schema
-          alt=""
-        />
+        {
+          picture?.asset?._ref &&
+          (
+            <Image
+              src={ urlForImage(picture).height(96).width(96).fit('crop').url() }
+              className="rounded-full"
+              height={96}
+              width={96}
+              alt={`Author's avatar: ${name}`}
+              title={`Author's avatar: ${name}`}
+            />
+          )
+        }
       </div>
       <div className="text-xl font-bold">{name}</div>
     </div>
