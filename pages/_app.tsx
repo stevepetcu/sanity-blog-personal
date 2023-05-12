f8ed2b88471442c94b67c2eefbdfdecd47fe4c4b
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-05-12 13:56:34 +0100

    📈 Add Vercel web analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import 'tailwindcss/tailwind.css'
 
 import { AppProps } from 'next/app'
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
