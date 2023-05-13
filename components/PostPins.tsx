f1d68a0b3edf83448081a2d04cd6fa5f5492bd02
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-05-13 23:48:47 +0100

    🚧 Work on the blog UI.

--- components/PostPins.tsx
+++ components/PostPins.tsx
@@ -8,7 +8,7 @@ export default function PostPins({ pins }: { pins: PostPin[] }) {
   return (
     <section>
       <h2 className={cn('text-xl antialiased mb-5')}>Pinned posts</h2>
-      <div className={cn('grid grid-cols-2 gap-y-10 md:grid-cols-3 md:gap-x-8 lg:gap-x-16')}>
+      <div className={cn('grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-8 lg:gap-x-16 lg:grid-cols-3')}>
         {pins.map((pin) => (
           <PostPinComponent
             key={pin._id}
