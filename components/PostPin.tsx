24c739e5ce95ba51803e38bdf4ae76a4bc2e5aa4
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-05-19 12:51:17 +0100

    💄 Style tweaks.

--- components/PostPin.tsx
+++ components/PostPin.tsx
@@ -27,24 +27,25 @@ export default function PostPin({
             width={384}
             height={99}
           />
-          <span className={cn('absolute top-1 left-1.5 font-bold text-white/75 text-sm lg:text-lg')}>
+          <span className={cn('absolute top-1 left-2 font-bold text-white/75 text-sm lg:text-lg')}>
             {index}/{total}
           </span>
         </div>
       }
+      {!coverImage &&
+        <div>
+          <span className={cn('ml-2 font-bold text-slate-400 text-sm lg:text-lg')}>{index}/{total}</span>
+        </div>
+      }
       <div className={cn('p-2')}>
-        {!coverImage &&
-          <span className={cn('font-bold text-slate-400 text-sm lg:text-lg')}>
-              {index}/{total}
-            </span>
-        }
         <h3 className={cn('leading-snug text-sm sm:text-base mb-3 font-medium text-slate-700')}>
           <Link href={`${POSTS_PAGE_PATH}/${slug}`} className='hover:underline'>
             {title}
           </Link>
         </h3>
         <div className={cn('flex flex-wrap')}>
-          {tags && tags.length && <TagList tags={tags} itemClassNames={'mr-1.5 mb-1.5 text-xs sm:text-sm lg:text-base'} />}
+          {tags && tags.length &&
+            <TagList tags={tags} itemClassNames={'mr-1.5 mb-1.5 text-xs sm:text-sm lg:text-base'} />}
         </div>
       </div>
     </div>
