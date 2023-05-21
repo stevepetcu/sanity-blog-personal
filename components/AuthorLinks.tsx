9f907586534ed518f7524d02b09fc126665eb017
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-05-21 13:00:55 +0100

    ✨ Update blog header and footer.

--- components/AuthorLinks.tsx
+++ components/AuthorLinks.tsx
@@ -1,5 +1,13 @@
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faFileCode } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFileCode,
+  faFlask,
+  faFlaskVial,
+  faIdBadge,
+  faVial,
+  faVials,
+  faVialVirus
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import cn from 'classnames'
 import Link from 'next/link'
@@ -7,30 +15,48 @@ import Link from 'next/link'
 import { Settings } from '../lib/sanity.queries'
 import { AuthorLinkedHandleWebsiteKeys } from '../schemas/author'
 
-export default function AuthorLinks({ admin, linkClassNames, placement }: Pick<Settings, 'admin'> & { linkClassNames?: string, placement: 'header' | 'footer' }) {
+export default function AuthorLinks({ admin, linkClassNames, placement }: Pick<Settings, 'admin'> & {
+  linkClassNames?: string,
+  placement: 'header' | 'footer'
+}) {
   const gitHubHandle = admin.handles.find(handle => handle.website === AuthorLinkedHandleWebsiteKeys.GITHUB)
   const linkedInHandle = admin.handles.find(handle => handle.website === AuthorLinkedHandleWebsiteKeys.LINKEDIN)
 
   return (
     <>
-      <Link href='#' className={cn(`shrink ml-2.5 sm:ml-3.5 md:ml-5 ${linkClassNames}`)}>About</Link>
-      <Link href='#' className={cn(`group shrink ml-2.5 sm:ml-3.5 md:ml-5 ${linkClassNames}`)}
-            title={`${admin.firstName}'s resume`}>
+      <Link href='#' className={cn(`group shrink ${linkClassNames}`)}
+            title={`${admin.firstName}'s about page and resume`}
+            aria-label={`${admin.firstName}'s about page and resume`}>
         <FontAwesomeIcon
-          icon={faFileCode}
-          aria-label={`${admin.firstName}'s resume`}
-          className={cn('w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 -rotate-12 hover:rotate-0 transition-all ease-in-out', {'origin-bottom-left': placement === 'header', 'origin-top-right': placement === 'footer'})} />
-        </Link>
+          icon={faIdBadge}
+          className={cn('w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 -rotate-6 hover:rotate-0 transition-all ease-in-out', {
+            'origin-bottom-left': placement === 'header',
+            'origin-top-right': placement === 'footer'
+          })} />
+      </Link>
+      <Link href='#' className={cn(`group shrink ml-5 ${linkClassNames}`)}
+            title={`${admin.firstName}'s code experiments`}
+            aria-label={`${admin.firstName}'s code experiments`}>
+        <FontAwesomeIcon
+          icon={faFlaskVial}
+          className={cn('w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 rotate-12 hover:rotate-0 transition-all ease-in-out', {
+            'origin-bottom-right': placement === 'header',
+            'origin-top-left': placement === 'footer'
+          })} />
+      </Link>
       {
         gitHubHandle &&
         (
           <Link href={`https://github.com/${gitHubHandle.name}`} target='_blank' rel='noopener'
-                className={cn(`shrink ml-2.5 sm:ml-3.5 md:ml-5 ${linkClassNames}`)}
-                title={`${admin.firstName}'s GitHub account`}>
+                className={cn(`shrink ml-5 ${linkClassNames}`)}
+                title={`${admin.firstName}'s GitHub account`}
+                aria-label={`${admin.firstName}'s GitHub account`}>
             <FontAwesomeIcon
               icon={faGithubSquare}
-              aria-label={`${admin.firstName}'s GitHub account`}
-              className={cn('w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 rotate-3 hover:rotate-0 transition-all ease-in-out', {'origin-bottom-right': placement === 'header', 'origin-top-left': placement === 'footer'})} />
+              className={cn('w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 rotate-3 hover:rotate-0 transition-all ease-in-out', {
+                'origin-bottom-right': placement === 'header',
+                'origin-top-left': placement === 'footer'
+              })} />
           </Link>
         )
       }
@@ -38,12 +64,15 @@ export default function AuthorLinks({ admin, linkClassNames, placement }: Pick<S
         linkedInHandle &&
         (
           <Link href={`https://linkedin.com/in/${linkedInHandle.name}`} target='_blank' rel='noopener'
-                className={cn(`shrink ml-2.5 sm:ml-3.5 md:ml-5 ${linkClassNames}`)}
-                title={`${admin.firstName}'s LinkedIn account`}>
+                className={cn(`shrink ml-2.5 ml-5 ${linkClassNames}`)}
+                title={`${admin.firstName}'s LinkedIn account`}
+                aria-label={`${admin.firstName}'s LinkedIn account`}>
             <FontAwesomeIcon
               icon={faLinkedin}
-              aria-label={`${admin.firstName}'s LinkedIn account`}
-              className={cn('w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 -rotate-6 hover:rotate-0 transition-all ease-in-out', {'origin-bottom-left': placement === 'header', 'origin-top-right': placement === 'footer'})} />
+              className={cn('w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 -rotate-6 hover:rotate-0 transition-all ease-in-out', {
+                'origin-bottom-left': placement === 'header',
+                'origin-top-right': placement === 'footer'
+              })} />
           </Link>
         )
       }
