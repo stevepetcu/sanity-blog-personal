2ea64eb8de262b942d8021318e29a2d2f4df1045
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2023-06-21 21:02:37 +0100

    🩹 Fix skip link non-hover colour for mobile.

--- components/SkipToMainContent.tsx
+++ components/SkipToMainContent.tsx
@@ -5,20 +5,23 @@ import cn from 'classnames';
 export default function SkipToMainContent() {
   return <a href={'#post-section-0'} title={'Skip to main content'}
     aria-label={'Skip to main content'}
-    className={'group opacity-20 hover:opacity-100 transition-opacity'}>
+    className={'group'}>
     <div className={'flex justify-center items-center ' +
     'space-x-2.5 w-full px-8'}>
       <div className={'group-hover:flex-grow'}>
-        <hr className="h-px my-1 bg-slate-400 border-0 w-28 group-hover:w-full"/>
+        <hr className="h-px my-1 bg-slate-200 group-hover:bg-slate-400
+        border-0 w-28 group-hover:w-full transition-colors"/>
       </div>
       <FontAwesomeIcon
         icon={faChevronDown}
         width={16}
         height={16}
-        className={cn('text-slate-500 my-2 group-hover:animate-bounce')}
+        className={cn('text-slate-300 group-hover:text-slate-500 ' +
+          'my-2 group-hover:animate-bounce transition-colors')}
       />
       <div className={'group-hover:flex-grow'}>
-        <hr className="h-px my-1 bg-slate-400 border-0 w-28 group-hover:w-full"/>
+        <hr className="h-px my-1 bg-slate-200 group-hover:bg-slate-400
+        border-0 w-28 group-hover:w-full transition-colors"/>
       </div>
     </div>
   </a>;
--- components/SkipToTitle.tsx
+++ components/SkipToTitle.tsx
@@ -5,20 +5,23 @@ import cn from 'classnames';
 export default function SkipToTitle() {
   return <a href={'#post-title'} title={'Skip to post title'}
     aria-label={'Skip to post title'}
-    className={'group opacity-20 hover:opacity-100 transition-opacity'}>
+    className={'group'}>
     <div className={'flex justify-center items-center ' +
       'space-x-2.5 w-full px-8'}>
       <div className={'group-hover:flex-grow'}>
-        <hr className="h-px my-1 bg-slate-400 border-0 w-28 group-hover:w-full"/>
+        <hr className="h-px my-1 bg-slate-200 group-hover:bg-slate-400
+        border-0 w-28 group-hover:w-full transition-colors"/>
       </div>
       <FontAwesomeIcon
         icon={faChevronUp}
         width={16}
         height={16}
-        className={cn('text-slate-500 my-2 group-hover:animate-bounce')}
+        className={cn('text-slate-300 group-hover:text-slate-500 ' +
+          'my-2 group-hover:animate-bounce transition-colors')}
       />
       <div className={'group-hover:flex-grow'}>
-        <hr className="h-px my-1 bg-slate-400 border-0 w-28 group-hover:w-full"/>
+        <hr className="h-px my-1 bg-slate-200 group-hover:bg-slate-400
+        border-0 w-28 group-hover:w-full transition-colors"/>
       </div>
     </div>
   </a>;
